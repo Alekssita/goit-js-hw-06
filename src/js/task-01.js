c92608851b8260d4dad31d30e714141243a79151
Alekssita <113428804+Alekssita@users.noreply.github.com>
2023-03-13 23:29:26 +0100

    js 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,9 @@
-const element = document.querySelectorAll('.item')
+
+const itemElement = document.querySelectorAll('.item');
+console.log('Number of categories:' , itemElement.length);
+itemElement.forEach(element => {
+    const titleEl = element.firstElementChild;
+    const innerLiEl = element.lastElementChild.children;
+    console.log('Category:' , titleEl.textContent);
+    console.log('Elements:' , innerLiEl.length);
+});
