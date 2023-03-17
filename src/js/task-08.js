90caa76ac7aaf93d92f7374f78ef0afe87b3f607
Alekssita <113428804+Alekssita@users.noreply.github.com>
2023-03-17 23:30:01 +0100

    tack 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    const inputValue = {
+        email,
+        password,
+    }
+   
+    console.log(`Email: ${email.value}, Password: ${password.value} `) 
+
+if (email.value === "" || password.value === "") {
+    window.alert("Всі поля повинні бути заповнені!");
+ }
+ event.target.reset()
+});
